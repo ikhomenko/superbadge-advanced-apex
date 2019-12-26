33187e564c0f600ef920e6ac33169f88c0be9dac
Ivan Khomenko <khomenko.ivan@gmail.com>
2019-12-26 16:48:58 +0200

    Step 1

---
+++ .sfdx/typings/lwc/customlabels.d.ts
@@ -0,0 +1,8 @@
+declare module "@salesforce/label/c.Inventory_Level_Low" {
+    var Inventory_Level_Low: string;
+    export default Inventory_Level_Low;
+}
+declare module "@salesforce/label/c.Select_One" {
+    var Select_One: string;
+    export default Select_One;
+}
